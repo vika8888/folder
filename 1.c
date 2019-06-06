d51497616a6c89f55531a8418c0fece140472099
Vika <vika83740@mail.ru>
2019-06-06 10:35:54 +0300

    -

--- 1.c
+++ 1.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main()
 {
-	int a,b,S;
+	int a,b,R,S;
 	printf("a= ");
 	scanf("%d", &a);
 	printf("b= ");
 	scanf("%d", &b);
 	S = a + b;
 	printf("S=%d", S);
+	R = a - b;
 	return 0;
 }
